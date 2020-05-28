18650cc3bd899e0fdb15d2c7b17546f82d629eb8
zhangjainxi <a867250586@yeah.net>
2020-05-28 16:39:16 +0800

    更改微信后台初始化

--- src/main/java/com/example/wechattest/api/service/impl/WechatServiceImpl.java
+++ src/main/java/com/example/wechattest/api/service/impl/WechatServiceImpl.java
@@ -4,8 +4,11 @@ package com.example.wechattest.api.service.impl;
 import com.example.wechattest.DataModel.WechatToken;
 import com.example.wechattest.Mapper.WechatMapper;
 import com.example.wechattest.api.service.WechatService;
+import com.example.wechattest.config.LogConfig;
 import com.example.wechattest.util.MyProps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,8 @@ import java.util.List;
 
 @Service
 public class WechatServiceImpl implements WechatService {
+    private static final Logger LOG = LoggerFactory.getLogger(LogConfig.class);
+
     @Autowired
     WechatMapper wechatMapper;
     @Autowired
@@ -27,12 +32,17 @@ public class WechatServiceImpl implements WechatService {
     public List<WechatToken> getAccessToken(){
         List<WechatToken> result = wechatMapper.getWechatToken();
         long nowTime = System.currentTimeMillis();
-        if(result.size()>0 && result.get(0)!=null){
-            long time = result.get(0).getTime();
-            if((nowTime-time)/1000>7200){
-                WechatToken wechatToken = this.updateAccessToken();
-                result.add(wechatToken);
+        LOG.info("获取accesstoken");
+        LOG.info(result.toString());
+        if(result!=null){
+            if(result.get(0)!=null){
+                long time = result.get(0).getTime();
+                if((nowTime-time)/1000>7200){
+                    WechatToken wechatToken = this.updateAccessToken();
+                    result.add(wechatToken);
+                }
             }
+
         }else{
             WechatToken wechatToken = this.insertAccessToken();
             result.add(wechatToken);
